0b78f648ee15d9fe6be0dfff3e7ebdb7c0484dd7
julienMP06 <102045381+julienMP06@users.noreply.github.com>
2022-04-17 20:32:35 +0200

    10.1 Bonus

--- src/VueJoueurs.java
+++ src/VueJoueurs.java
@@ -3,14 +3,15 @@ import java.awt.*;
 public class VueJoueurs extends JPanel{
     private static CModele modele;
     private JPanel panelJ = new JPanel();
-    GridLayout grid = new GridLayout(4, 0);
 
-    Font font = new Font("Courier", Font.BOLD,12);
+    private int x = 0;
+    GridLayout grid = new GridLayout(5, 0);
 
     private JLabel label = new JLabel();
     private JLabel label1 = new JLabel();
     private JLabel label2 = new JLabel();
     private JLabel label3 = new JLabel();
+    private JLabel label4 = new JLabel();
 
     public VueJoueurs (CModele modele){
         this.modele=modele;
@@ -19,17 +20,20 @@ public class VueJoueurs extends JPanel{
         JoueurPanneau(panelJ);
     }
     public void JoueurPanneau(JPanel p){
-        Font f = label.getFont();
-        label.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
+        x = x +1;
         label.setText("%s   clé : %s   Artefacts : %d   Action : %d".formatted(modele.getJoueurs().get(0).getNom_joueur(), modele.getJoueurs().get(0).getNb_cleE(), modele.getJoueurs().get(0).getArtefacts(), modele.getJoueurs().get(0).getNb_act()));
         label1.setText(modele.getJoueurs().get(1).getNom_joueur()+"   clé : "+modele.getJoueurs().get(1).getNb_cleE()+"   Artefacts : "+modele.getJoueurs().get(1).getArtefacts()+"   Action : "+modele.getJoueurs().get(1).getNb_act());
         label2.setText(modele.getJoueurs().get(2).getNom_joueur()+"   clé : "+modele.getJoueurs().get(2).getNb_cleE()+"   Artefacts : "+modele.getJoueurs().get(2).getArtefacts()+"   Action : "+modele.getJoueurs().get(2).getNb_act());
         label3.setText(modele.getJoueurs().get(3).getNom_joueur()+"   clé : "+modele.getJoueurs().get(3).getNb_cleE()+"   Artefacts : "+modele.getJoueurs().get(3).getArtefacts()+"   Action : "+modele.getJoueurs().get(3).getNb_act());
+        label4.setText(String.valueOf(x));
+        p.add(label4);
         p.add(label);
         p.add(label1);
         p.add(label2);
         p.add(label3);
+        p.add(label4);
         this.add(p);
+        System.out.println(x);
     }
     public void MajJpanel(){
         JoueurPanneau(panelJ);
