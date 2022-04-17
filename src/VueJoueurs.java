7f5ddd49b20165c8af29c9985b7515edf83c5de5
julienMP06 <102045381+julienMP06@users.noreply.github.com>
2022-04-17 18:36:07 +0200

    JLabel

--- src/VueJoueurs.java
+++ src/VueJoueurs.java
@@ -2,12 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class VueJoueurs extends JPanel{
-
     private static CModele modele;
-
-    private int x =0;
     private JPanel panel = new JPanel();
-    GridLayout grid = new GridLayout(4, 0);
+    GridLayout grid = new GridLayout(4, 4);
 
     JLabel label = new JLabel();
     JLabel label1 = new JLabel();
@@ -17,22 +14,19 @@ public class VueJoueurs extends JPanel{
     public VueJoueurs (CModele modele){
         this.modele=modele;
         setLayout(new BorderLayout());
-        //panel.setLayout(grid);
+        panel.setLayout(grid);
         JoueurPanneau(panel);
     }
     public void JoueurPanneau(JPanel panel){
-        x = x+1;
         label.setText("%s   clé : %s   Artefacts : %d   Action : %d".formatted(modele.getJoueurs().get(0).getNom_joueur(), modele.getJoueurs().get(0).getNb_cle(), modele.getJoueurs().get(0).getArtefacts(), modele.getJoueurs().get(0).getNb_act()));
         label1.setText(modele.getJoueurs().get(1).getNom_joueur()+"   clé : "+modele.getJoueurs().get(1).getNb_cle()+"   Artefacts : "+modele.getJoueurs().get(1).getArtefacts()+"   Action : "+modele.getJoueurs().get(1).getNb_act());
         label2.setText(modele.getJoueurs().get(2).getNom_joueur()+"   clé : "+modele.getJoueurs().get(2).getNb_cle()+"   Artefacts : "+modele.getJoueurs().get(2).getArtefacts()+"   Action : "+modele.getJoueurs().get(2).getNb_act());
         label3.setText(modele.getJoueurs().get(3).getNom_joueur()+"   clé : "+modele.getJoueurs().get(3).getNb_cle()+"   Artefacts : "+modele.getJoueurs().get(3).getArtefacts()+"   Action : "+modele.getJoueurs().get(3).getNb_act());
-        label3.setText(String.valueOf(x));
-        System.out.println(x);
         panel.add(label);
         panel.add(label1);
         panel.add(label2);
         panel.add(label3);
-        this.add(label3);
+        this.add(panel);
     }
     public void MajJpanel(){
         JoueurPanneau(panel);
